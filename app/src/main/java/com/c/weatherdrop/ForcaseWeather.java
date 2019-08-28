b83a8b7f8855d8dfab11eaf1b1d80985ccd5fce4
s4daf <sadaf.farhod@gmail.com>
2019-08-28 12:03:44 +0430

    gg

---
+++ app/src/main/java/com/c/weatherdrop/ForcaseWeather.java
@@ -0,0 +1,15 @@
+package com.c.weatherdrop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class ForcaseWeather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forcase_weather);
+    }
+}
---
+++ app/src/main/java/com/c/weatherdrop/ItemAdapterActivity.java
@@ -0,0 +1,14 @@
+package com.c.weatherdrop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemAdapterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_adapter);
+    }
+}
